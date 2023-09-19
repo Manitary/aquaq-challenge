e13e8b4d83258a69cb558da5162d2939b882f6d8
Manitary <pak.ska@gmail.com>
2023-09-19 07:23:29 +0100

    Solve challenge 1

---
+++ src/p1.py
@@ -0,0 +1,18 @@
+from utils import get_input, submit
+
+HEX = "0123456789abcdef"
+
+
+def main() -> str:
+    data = get_input(1).strip()
+    data += " " * ((3 - (len(data) % 3)) % 3)
+    data = "".join(c if c in HEX else "0" for c in data)
+    return "".join(
+        data[i * (len(data) // 3) : i * (len(data) // 3) + 2] for i in range(3)
+    )
+
+
+if __name__ == "__main__":
+    ANSWER = main()
+    print(f"Answer: {ANSWER}")
+    submit(1, ANSWER)
