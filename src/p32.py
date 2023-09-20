2299b2abc42ed999a3eb3e8d2fa2590180fcf7ad
Manitary <pak.ska@gmail.com>
2023-09-20 03:52:54 +0100

    Solve challenge 32

---
+++ src/p32.py
@@ -0,0 +1,30 @@
+from utils import get_input, submit
+
+BRACES = set("()[]{}")
+CLOSE = {")": "(", "]": "[", "}": "{"}
+
+
+def is_balanced(s: str) -> bool:
+    stack: list[str] = []
+    for c in s:
+        if c not in BRACES:
+            continue
+        if c not in CLOSE:
+            stack.append(c)
+            continue
+        if not stack or stack.pop() != CLOSE[c]:
+            return False
+    if stack:
+        return False
+    return True
+
+
+def main() -> int:
+    data = get_input(32).strip().split("\n")
+    return sum(is_balanced(row) for row in data)
+
+
+if __name__ == "__main__":
+    ANSWER = main()
+    print(f"Answer: {ANSWER}")
+    submit(32, ANSWER)
