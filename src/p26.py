f278df3c5cc8824f8b1e98ac8a737f26ab577a38
Manitary <pak.ska@gmail.com>
2023-09-21 03:04:09 +0100

    Solve challenge 26

---
+++ src/p26.py
@@ -0,0 +1,24 @@
+from utils import get_input, submit
+
+
+def next_larger_permutation(n: str) -> str:
+    l = list(n)
+    for i in range(len(l) - 2, -1, -1):
+        if all(x <= l[i] for x in l[i + 1 :]):
+            continue
+        tail = l[i:]
+        head = min(x for x in tail[1:] if x > tail[0])
+        tail.remove(head)
+        return "".join(l[:i] + [head] + sorted(tail))
+    return n
+
+
+def main() -> int:
+    data = get_input(26).strip().split("\n")
+    return sum(int(next_larger_permutation(d)) - int(d) for d in data)
+
+
+if __name__ == "__main__":
+    ANSWER = main()
+    print(f"Answer: {ANSWER}")
+    submit(26, ANSWER)
