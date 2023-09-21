6ad8da62d310841733d31abdc73a62a3f6e12df3
Manitary <pak.ska@gmail.com>
2023-09-21 05:45:16 +0100

    Solve challenge 19
    Still slow on the last input, but it works

--- src/p19.py
+++ src/p19.py
@@ -1,39 +1,36 @@
-from itertools import product
-from typing import Generator
+import numpy as np
+from numpy.typing import NDArray
+
 from utils import get_input, submit
 
 
-def ngbh(x: int, y: int) -> Generator[tuple[int, int], None, None]:
-    for i in (-1, 1):
-        yield (x + i, y)
-        yield (x, y + i)
-
-
-def play_turn(board: dict[tuple[int, int], int]) -> dict[tuple[int, int], int]:
-    new_board = {
-        (x, y): sum(board.get(coords, 0) for coords in ngbh(x, y)) % 2 for x, y in board
-    }
-    return new_board
+def play_turn(board: NDArray[np.int64]) -> NDArray[np.int64]:
+    new_board = np.pad(board, ((1, 1), (1, 1)))
+    return (
+        sum(
+            (np.roll(new_board, i, j) for i in (-1, 1) for j in (0, 1)),
+            start=np.zeros(shape=new_board.shape, dtype=np.int64),
+        )
+        % 2
+    )[1:-1, 1:-1]
 
 
 def play_game(num_steps: int, size: int, *coords: int) -> int:
-    board = {(x, y): 0 for x, y in product(range(size), repeat=2)}
+    board = np.zeros((size, size), dtype=np.int64)
     for x, y in zip(coords[::2], coords[1::2]):
-        board[(x, y)] = 1
+        board[x, y] = 1
     for _ in range(num_steps):
         board = play_turn(board)
-    ans = sum(board.values())
-    print(num_steps, size, ans)
-    return ans
+    ans = np.sum(board)
+    return int(ans)
 
 
 def main() -> int:
     data = tuple(tuple(map(int, row.split())) for row in get_input(19).split("\n"))
-    # data = [[int(x) for x in "350 6 2 2 2 3".split()]]
     return sum(play_game(*setup) for setup in data)
 
 
 if __name__ == "__main__":
     ANSWER = main()
     print(f"Answer: {ANSWER}")
-    # submit(19, ANSWER)
+    submit(19, ANSWER)
